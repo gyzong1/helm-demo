88169627580539866f1fa5cb92ef0a93c59fd833
gyzong1 <40457486+gyzong1@users.noreply.github.com>
2020-10-27 13:16:02 +0800

    Update HomeController.java

--- src/main/java/com/jfrog/cloud/HomeController.java
+++ src/main/java/com/jfrog/cloud/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @RequestMapping("/")
     public String hello() {
-        return "Welcome to JFrog Artifactory!!!";
+        return "Welcome to JFrog Artifactory!";
     }
 }
